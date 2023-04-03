c5fdbfe54289bdf2983a7950db9112ca8d4c3ee8
David Jandron <jandron1211@gmail.com>
2023-04-03 13:33:37 -0700

    ADD database update

---
+++ HiddenGarden/Migrations/20230403202300_Initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiddenGarden.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Backyards",
+                columns: table => new
+                {
+                    BackyardId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Service = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Address = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Latitude = table.Column<float>(type: "float", nullable: false),
+                    Longitude = table.Column<float>(type: "float", nullable: false),
+                    Instructions = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Backyards", x => x.BackyardId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Backyards",
+                columns: new[] { "BackyardId", "Address", "Description", "Instructions", "Latitude", "Longitude", "Service" },
+                values: new object[] { 1, "13704 SE Salmon St, Portland OR, 97233", "Free fig tree fruit", "Pay $5, follow road signs", 0f, 0f, "Fig trees" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Backyards");
+        }
+    }
+}
--- HiddenGarden/Models/HiddenGardenContext.cs
+++ HiddenGarden/Models/HiddenGardenContext.cs
@@ -14,7 +14,8 @@ namespace HiddenGarden.Models
     {
       builder.Entity<Backyard>()
         .HasData(
-          new Backyard { BackyardId = 1, Service = "Fig trees", Description = "Free fig tree fruit", Address = "13704 SE Salmon St, Portland OR, 97233" , Instructions = "Pay $5, follow road signs"}
+          new Backyard { BackyardId = 1, Service = "Fig trees", Description = "Free fig tree fruit", Address = "13704 SE Salmon St, Portland OR, 97233" , Instructions = "Pay $5, follow road signs"},
+          new Backyard { BackyardId = 2, Service = "Apple Trees", Description = "Free Apples", Address = "Bennelong Point, Sydney NSW 2000, Australia", Instructions = "Ask the receptionist for apples!"}
         );
     }
   }
