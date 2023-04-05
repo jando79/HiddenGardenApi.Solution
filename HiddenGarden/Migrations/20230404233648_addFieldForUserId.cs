ed1a76a5e28f51e1cc2cf3f08b5fdfc8333d11bd
Kai Clausen <kaiclausen123@gmail.com>
2023-04-05 08:24:23 -0700

    add migration

---
+++ HiddenGarden/Migrations/20230404233648_addFieldForUserId.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiddenGarden.Migrations
+{
+    public partial class addFieldForUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Backyards",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Backyards",
+                keyColumn: "BackyardId",
+                keyValue: 3,
+                column: "Instructions",
+                value: "Come up to the window and ask for Pierre!");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Backyards");
+
+            migrationBuilder.UpdateData(
+                table: "Backyards",
+                keyColumn: "BackyardId",
+                keyValue: 3,
+                column: "Instructions",
+                value: "Come up to the window and ask for knock!");
+        }
+    }
+}
