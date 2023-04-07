40ded9fabb5f3b02f4a6a6f70d80d0d335adba26
David Jandron <jandron1211@gmail.com>
2023-04-07 05:34:45 -0700

    ADD migrations and committing to pull changes

---
+++ HiddenGarden/Migrations/20230404164417_AddPierre.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiddenGarden.Migrations
+{
+    public partial class AddPierre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Backyards",
+                columns: new[] { "BackyardId", "Address", "Description", "Instructions", "Latitude", "Longitude", "Service" },
+                values: new object[] { 2, "Bennelong Point, Sydney NSW 2000, Australia", "Free Apples", "Ask the receptionist for apples!", 0f, 0f, "Apple Trees" });
+
+            migrationBuilder.InsertData(
+                table: "Backyards",
+                columns: new[] { "BackyardId", "Address", "Description", "Instructions", "Latitude", "Longitude", "Service" },
+                values: new object[] { 3, "Pierre’s Bakery, Portland, OR", "Day after baked goods", "Come up to the window and ask for knock!", 0f, 0f, "Bread and Pastries" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Backyards",
+                keyColumn: "BackyardId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Backyards",
+                keyColumn: "BackyardId",
+                keyValue: 3);
+        }
+    }
+}
